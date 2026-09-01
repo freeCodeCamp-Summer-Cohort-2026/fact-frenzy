e38385eaadf9400adf4abbc583d4bcb666743715
ofthewildfire <103148299+ofthewildfire@users.noreply.github.com>
2026-09-02 00:06:25 +0200

    Resolution: modified function

--- backend/main.py
+++ backend/main.py
@@ -7,5 +7,6 @@ def main():
     return {"message": "Hello from backend!"}
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
