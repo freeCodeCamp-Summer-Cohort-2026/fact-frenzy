7fafc1663a4ae34466ce65da1db96635bb7613a6
Ahmed Omer <a.omer46@yahoo.co.uk>
2026-09-10 20:09:00 +0000

    chore: add password field to user model (#69)

--- backend/app/seed.py
+++ backend/app/seed.py
@@ -4,11 +4,36 @@ from database import create_db_table, engine
 from models import User
 
 USERS = [
-    {"name": "Alice Johnson", "email": "alice.johnson@example.com", "is_admin": True},
-    {"name": "Bob Smith", "email": "b.smith@testcorp.net", "is_admin": False},
-    {"name": "Charlie Brown", "email": "charlie_b@mail.org", "is_admin": False},
-    {"name": "Dana", "email": "dana@fastapi.io", "is_admin": False},
-    {"name": "Dr. Evelyn Reed-Jones", "email": "evelyn.rj@research.edu", "is_admin": True},
+    {
+        "name": "Alice Johnson",
+        "email": "alice.johnson@example.com",
+        "is_admin": True,
+        "hashed_password": "asdfghjkl",
+    },
+    {
+        "name": "Bob Smith",
+        "email": "b.smith@testcorp.net",
+        "is_admin": False,
+        "hashed_password": "123456",
+    },
+    {
+        "name": "Charlie Brown",
+        "email": "charlie_b@mail.org",
+        "is_admin": False,
+        "hashed_password": "qwerty",
+    },
+    {
+        "name": "Dana",
+        "email": "dana@fastapi.io",
+        "is_admin": False,
+        "hashed_password": "wasdwasd",
+    },
+    {
+        "name": "Dr. Evelyn Reed-Jones",
+        "email": "evelyn.rj@research.edu",
+        "is_admin": True,
+        "hashed_password": "lmnopqrs",
+    },
 ]
 
 
